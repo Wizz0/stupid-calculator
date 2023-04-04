3f27818188bb0b903d7d6bc5461287366e34e5c8
Wizz0 <samgorokhov01@gmail.com>
2023-04-04 19:43:10 +0900

    Create stupid-calc.cpp
    Just a start of programm.,,

---
+++ stupid-calc.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+double add(){ //сложение
+    
+}
+
+double subtr(){ //вычитание
+    
+}
+
+double multipl(){ //умножение
+    
+}
+
+double div(){ //деление
+    
+}
+
+void check(){ //ввод и проверка корректности (void?)
+    
+}
+
+void printRes(){ //вывод
+    
+}
+
+int main(){
+    
+}
